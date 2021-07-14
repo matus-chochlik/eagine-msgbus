c5abefce66de7f93d5cde606acc5ed0c9b49f486
Matus Chochlik <chochlik@gmail.com>
2021-07-14 18:22:33 +0200

    Added doxygen and sphinx documentation sources + updated sub-modules

---
+++ doc/doxygen/include/msgbus.hpp
@@ -0,0 +1,16 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+namespace eagine {
+
+/// @brief Message bus code is placed in this namespace.
+namespace msgbus {
+/// @defgroup msgbus Message bus
+}
+
+} // namespace eagine
