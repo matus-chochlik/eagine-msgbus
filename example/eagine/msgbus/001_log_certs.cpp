af1ab8184e5760aa7a92a29b0c9a4c943f45ab8c
Matus Chochlik <chochlik@gmail.com>
2021-08-17 19:29:23 +0200

    Fixed doxygen doc comment

--- example/eagine/msgbus/001_log_certs.cpp
+++ example/eagine/msgbus/001_log_certs.cpp
@@ -1,4 +1,4 @@
-/// @example eagine/sslplus/001_list_engines.cpp
+/// @example eagine/msgbus/001_log_certs.cpp
 ///
 /// Copyright Matus Chochlik.
 /// Distributed under the Boost Software License, Version 1.0.
