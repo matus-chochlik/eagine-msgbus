d0843832702a94f135131317e42033a3df7005a9
Matus Chochlik <chochlik@gmail.com>
2022-09-10 08:20:18 +0200

    Added new example (W.I.P.)

---
+++ example/eagine/msgbus/006_stream_histogram.cpp
@@ -0,0 +1,48 @@
+/// @example eagine/msgbus/006_stream_histogram.cpp
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+import eagine.core;
+import eagine.sslplus;
+import eagine.msgbus;
+import <chrono>;
+import <thread>;
+
+namespace eagine {
+
+auto main(main_ctx& ctx) -> int {
+    enable_message_bus(ctx);
+
+    timeout idle_too_long{std::chrono::seconds{30}};
+
+    msgbus::resource_data_consumer_node node{ctx};
+    msgbus::setup_connectors(ctx, node);
+
+    const auto is_done = [&] {
+        if(idle_too_long) {
+            return true;
+        }
+        return false;
+    };
+
+    while(!is_done()) {
+        if(node.update_and_process_all()) {
+            idle_too_long.reset();
+        } else {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+    }
+
+    return 0;
+}
+
+} // namespace eagine
+
+auto main(int argc, const char** argv) -> int {
+    eagine::main_ctx_options options;
+    options.app_id = "RsrcExmple";
+    return eagine::main_impl(argc, argv, options, eagine::main);
+}
