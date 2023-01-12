1968876e7f72d0f2ece13711ddb09eca434ea35b
Matus Chochlik <chochlik@gmail.com>
2023-01-12 06:45:30 +0100

    Added test for registry

---
+++ source/modules/eagine/core/registry_test.cpp
@@ -0,0 +1,171 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+#include <eagine/testing/unit_begin_ctx.hpp>
+#include <chrono>
+import eagine.core;
+import eagine.msgbus.core;
+import <thread>;
+import <chrono>;
+//------------------------------------------------------------------------------
+// ping / pong
+//------------------------------------------------------------------------------
+template <typename Base = eagine::msgbus::subscriber>
+class test_pong : public Base {
+public:
+    void assign(eagitest::track& trck) noexcept {
+        _ptrck = &trck;
+    }
+
+protected:
+    using Base::Base;
+
+    void add_methods() noexcept {
+        Base::add_methods();
+        Base::add_method(
+          this,
+          eagine::msgbus::
+            message_map<"eagiTest", "ping", &test_pong::_handle_ping>{});
+    }
+
+private:
+    auto _handle_ping(
+      const eagine::msgbus::message_context&,
+      const eagine::msgbus::stored_message& message) noexcept -> bool {
+        if(_ptrck) {
+            _ptrck->checkpoint(1);
+        }
+        this->bus_node().respond_to(
+          message, eagine::message_id{"eagiTest", "pong"}, {});
+        return true;
+    }
+
+    eagitest::track* _ptrck{nullptr};
+};
+//------------------------------------------------------------------------------
+template <typename Base = eagine::msgbus::subscriber>
+class test_ping : public Base {
+public:
+    void assign(eagitest::track& trck) noexcept {
+        _ptrck = &trck;
+    }
+
+    void assign_target(eagine::identifier_t id) noexcept {
+        _target = id;
+    }
+
+    auto success() const noexcept -> bool {
+        return _rcvd >= _max;
+    }
+
+protected:
+    using Base::Base;
+
+    void add_methods() noexcept {
+        Base::add_methods();
+        Base::add_method(
+          this,
+          eagine::msgbus::
+            message_map<"eagiTest", "pong", &test_ping::_handle_pong>{});
+    }
+    auto update() -> eagine::work_done {
+        eagine::some_true something_done{Base::update()};
+        if(eagine::msgbus::is_valid_endpoint_id(_target)) {
+            if(_ping_time.is_expired()) {
+                eagine::msgbus::message_view ping_msg;
+                ping_msg.set_target_id(_target);
+                ping_msg.set_sequence_no(_seq_id);
+                this->bus_node().post({"eagiTest", "ping"}, ping_msg);
+                _ping_time.reset();
+                something_done();
+            }
+        }
+        return something_done;
+    }
+
+private:
+    auto _handle_pong(
+      const eagine::msgbus::message_context&,
+      const eagine::msgbus::stored_message&) noexcept -> bool {
+        if(_ptrck) {
+            _ptrck->checkpoint(2);
+        }
+        ++_rcvd;
+        return true;
+    }
+
+    int _max{5000};
+    int _sent{0};
+    int _rcvd{0};
+    eagine::msgbus::message_sequence_t _seq_id{0};
+    eagine::timeout _ping_time{std::chrono::milliseconds{1}};
+    eagine::identifier_t _target{eagine::msgbus::invalid_endpoint_id()};
+    eagitest::track* _ptrck{nullptr};
+};
+//------------------------------------------------------------------------------
+void registry_ping_pong(auto& s) {
+    eagitest::case_ test{s, 1, "ping-pong"};
+    eagitest::track trck{test, 0, 4};
+    auto& ctx{s.context()};
+    eagine::msgbus::registry the_reg{ctx};
+
+    auto& ponger = the_reg.emplace<eagine::msgbus::service_composition<
+      eagine::msgbus::require_services<eagine::msgbus::subscriber, test_pong>>>(
+      "TestPong");
+    auto& pinger = the_reg.emplace<eagine::msgbus::service_composition<
+      eagine::msgbus::require_services<eagine::msgbus::subscriber, test_ping>>>(
+      "TestPing");
+
+    ponger.assign(trck);
+    pinger.assign(trck);
+
+    eagine::timeout get_id_time{std::chrono::minutes{1}};
+    while(not(ponger.bus_node().has_id() and pinger.bus_node().has_id())) {
+        if(get_id_time.is_expired()) {
+            test.fail("get-id timeout");
+            break;
+        }
+        if(not the_reg.update_all()) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+        trck.checkpoint(3);
+    }
+
+    if(ponger.bus_node().has_id()) {
+        pinger.assign_target(ponger.bus_node().get_id());
+
+        eagine::timeout ping_time{std::chrono::minutes{1}};
+        while(not pinger.success()) {
+            if(ping_time.is_expired()) {
+                test.fail("ping timeout");
+                break;
+            }
+            if(not the_reg.update_all()) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+            trck.checkpoint(4);
+        }
+    }
+};
+//------------------------------------------------------------------------------
+// main
+//------------------------------------------------------------------------------
+auto test_main(eagine::test_ctx& ctx) -> int {
+    enable_message_bus(ctx);
+    ctx.preinitialize();
+
+    eagitest::ctx_suite test{ctx, "registry", 1};
+    test.once(registry_ping_pong);
+    return test.exit_code();
+}
+//------------------------------------------------------------------------------
+auto main(int argc, const char** argv) -> int {
+    return eagine::test_main_impl(argc, argv, test_main);
+}
+//------------------------------------------------------------------------------
+#include <eagine/testing/unit_end_ctx.hpp>
