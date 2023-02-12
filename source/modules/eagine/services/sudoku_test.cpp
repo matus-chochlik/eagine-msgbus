5ed98c321916e89125610d95591a88c14d79a80f
Matus Chochlik <chochlik@gmail.com>
2023-02-12 16:11:40 +0100

    Updated tests for the Sudoku services

--- source/modules/eagine/services/sudoku_test.cpp
+++ source/modules/eagine/services/sudoku_test.cpp
@@ -62,22 +62,17 @@ void sudoku_rank_S_1(auto& s, auto& test) {
         if(the_reg.wait_for_id_of(std::chrono::seconds{30}, solver)) {
             solver.assign_track(trck);
 
-            for(unsigned r = 0; r < test.repeats(3); ++r) {
-                solver.enqueue(
-                  0,
-                  eagine::default_sudoku_board_traits<S>()
-                    .make_generator()
-                    .generate_medium());
+            solver.enqueue(
+              0, eagine::default_sudoku_board_traits<S>().make_diagonal());
 
-                eagine::timeout solution_timeout{std::chrono::minutes{1}};
-                while(not solver.is_done()) {
-                    if(solution_timeout.is_expired()) {
-                        test.fail("receive timeout");
-                        break;
-                    }
-                    the_reg.update_all();
-                    trck.checkpoint(2);
+            eagine::timeout solution_timeout{std::chrono::minutes{1}};
+            while(not solver.is_done()) {
+                if(solution_timeout.is_expired()) {
+                    test.fail("receive timeout");
+                    break;
                 }
+                the_reg.update_all();
+                trck.checkpoint(2);
             }
             trck.checkpoint(3);
         } else {
@@ -102,15 +97,83 @@ void sudoku_rank_4_1(auto& s) {
     sudoku_rank_S_1<4>(s, test);
 }
 //------------------------------------------------------------------------------
+// test 2
+//------------------------------------------------------------------------------
+template <unsigned S>
+void sudoku_rank_S_2(auto& s, auto& test) {
+    eagitest::track trck{test, 0, 4};
+    auto& ctx{s.context()};
+    eagine::msgbus::registry the_reg{ctx};
+
+    auto& helper = the_reg.emplace<
+      eagine::msgbus::service_composition<eagine::msgbus::sudoku_helper<>>>(
+      "Helper");
+
+    if(the_reg.wait_for_id_of(std::chrono::seconds{30}, helper)) {
+        auto& solver =
+          the_reg.emplace<eagine::msgbus::service_composition<test_solver<>>>(
+            "Solver");
+
+        if(the_reg.wait_for_id_of(std::chrono::seconds{30}, solver)) {
+            solver.assign_track(trck);
+
+            int todo{3};
+            while(todo > 0) {
+                solver.enqueue(
+                  0,
+                  eagine::default_sudoku_board_traits<S>()
+                    .make_generator()
+                    .generate_medium());
+
+                eagine::timeout solution_timeout{std::chrono::seconds{15}};
+                while(not solver.is_done()) {
+                    if(solution_timeout.is_expired()) {
+                        break;
+                    }
+                    the_reg.update_all();
+                    trck.checkpoint(2);
+                }
+                if(solver.is_done()) {
+                    --todo;
+                }
+            }
+            if(todo > 0) {
+                test.fail("solution timeout");
+            }
+            trck.checkpoint(3);
+        } else {
+            test.fail("get id solver");
+        }
+
+        trck.checkpoint(4);
+    } else {
+        test.fail("get id helper");
+    }
+
+    the_reg.finish();
+}
+//------------------------------------------------------------------------------
+void sudoku_rank_3_2(auto& s) {
+    eagitest::case_ test{s, 3, "rank 3"};
+    sudoku_rank_S_2<3>(s, test);
+}
+//------------------------------------------------------------------------------
+void sudoku_rank_4_2(auto& s) {
+    eagitest::case_ test{s, 4, "rank 4"};
+    sudoku_rank_S_2<4>(s, test);
+}
+//------------------------------------------------------------------------------
 // main
 //------------------------------------------------------------------------------
 auto test_main(eagine::test_ctx& ctx) -> int {
     enable_message_bus(ctx);
     ctx.preinitialize();
 
-    eagitest::ctx_suite test{ctx, "sudoku", 2};
+    eagitest::ctx_suite test{ctx, "sudoku", 4};
     test.once(sudoku_rank_3_1);
     test.once(sudoku_rank_4_1);
+    test.once(sudoku_rank_3_2);
+    test.once(sudoku_rank_4_2);
     return test.exit_code();
 }
 //------------------------------------------------------------------------------
