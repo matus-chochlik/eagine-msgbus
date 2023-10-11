2e66c1adbde40fd79a9fa899cbaf8026b109bbbf
Matus Chochlik <chochlik@gmail.com>
2023-10-11 06:47:33 +0200

    Added copyright notice to a helper script

--- devel/procman.sh
+++ devel/procman.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Copyright Matus Chochlik.
+# Distributed under the Boost Software License, Version 1.0.
+# See accompanying file LICENSE_1_0.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt
+#
 if [[ -x "$(dirname ${0})/../../eagine-core/source/app/procman.py" ]]
 then procman="$(dirname ${0})/../../eagine-core/source/app/procman.py"
 elif [[ -x "$(dirname ${0})/../submodules/eagine-core/source/app/procman.py" ]]
