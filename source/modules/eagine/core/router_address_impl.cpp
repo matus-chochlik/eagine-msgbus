47b28b6ee5924b2f85b0defbd8bb1552c848d42e
Matus Chochlik <chochlik@gmail.com>
2023-11-06 07:59:22 +0100

    Moved some router_address functions to implementation file

---
+++ source/modules/eagine/core/router_address_impl.cpp
@@ -0,0 +1,39 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+module;
+module eagine.msgbus.core;
+
+import std;
+import eagine.core.types;
+import eagine.core.identifier;
+import eagine.core.logging;
+import eagine.core.main_ctx;
+
+namespace eagine::msgbus {
+//------------------------------------------------------------------------------
+router_address::router_address(main_ctx_parent parent, const nothing_t) noexcept
+  : main_ctx_object{"RouterAddr", parent} {}
+//------------------------------------------------------------------------------
+router_address::router_address(main_ctx_parent parent) noexcept
+  : router_address{parent, nothing} {
+    configure(app_config());
+}
+//------------------------------------------------------------------------------
+void router_address::configure(application_config& config) {
+    if(config.fetch("msgbus.router.address", _addrs)) {
+        log_info("configured router address(es) ${address}")
+          .arg_func([&](logger_backend& backend) {
+              for(auto& addr : _addrs) {
+                  backend.add_string("address", "string", addr);
+              }
+          });
+    }
+}
+//------------------------------------------------------------------------------
+} // namespace eagine::msgbus
+
