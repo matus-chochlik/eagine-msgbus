11083868a3ba08ea5d05a2017d842134ebfcf500
Matus Chochlik <chochlik@gmail.com>
2024-03-23 08:35:14 +0100

    Refactoring in the shutdown services implementation

--- source/modules/eagine/services/shutdown_impl.cpp
+++ source/modules/eagine/services/shutdown_impl.cpp
@@ -23,57 +23,70 @@ public:
       : base{sub}
       , signals{sigs} {}
 
-    void add_methods() noexcept final {
-        base.add_method(
-          this,
-          message_map<
-            "Shutdown",
-            "shutdown",
-            &shutdown_target_impl::_handle_shutdown>{});
-    }
+    void add_methods() noexcept final;
 
     auto do_decode_shutdown_request(
       const message_context& msg_ctx,
       const stored_message& message) noexcept
-      -> std::optional<shutdown_request> {
-        typename shutdown_service_duration::rep count{0};
-        if(default_deserialize(count, message.content())) {
-            const shutdown_service_duration ticks{count};
-            const typename shutdown_service_clock::time_point ts{ticks};
-            const auto age{this->now() - ts};
-            return {shutdown_request{
-              .source_id = message.source_id,
-              .age = std::chrono::duration_cast<std::chrono::milliseconds>(age),
-              .verified = base.verify_bits(message)}};
-        }
-        return {};
-    }
+      -> std::optional<shutdown_request>;
 
     auto decode_shutdown_request(
       const message_context& msg_ctx,
       const stored_message& message) noexcept
-      -> std::optional<shutdown_request> final {
-        if(msg_ctx.msg_id().is("Shutdown", "shutdown")) {
-            return do_decode_shutdown_request(msg_ctx, message);
-        }
-        return {};
-    }
+      -> std::optional<shutdown_request> final;
 
 private:
     auto _handle_shutdown(
       const message_context& msg_ctx,
-      const stored_message& message) noexcept -> bool {
-        // TODO: and_then when 23 is available
-        if(const auto decoded{do_decode_shutdown_request(msg_ctx, message)}) {
-            signals.shutdown_requested(
-              result_context{msg_ctx, message}, *decoded);
-        }
-        return true;
-    }
+      const stored_message& message) noexcept -> bool;
 
     subscriber& base;
     shutdown_target_signals& signals;
 };
+//------------------------------------------------------------------------------
+auto shutdown_target_impl::_handle_shutdown(
+  const message_context& msg_ctx,
+  const stored_message& message) noexcept -> bool {
+    // TODO: and_then when 23 is available
+    if(const auto decoded{do_decode_shutdown_request(msg_ctx, message)}) {
+        signals.shutdown_requested(result_context{msg_ctx, message}, *decoded);
+    }
+    return true;
+}
+//------------------------------------------------------------------------------
+void shutdown_target_impl::add_methods() noexcept {
+    base.add_method(
+      this,
+      message_map<
+        "Shutdown",
+        "shutdown",
+        &shutdown_target_impl::_handle_shutdown>{});
+}
+//------------------------------------------------------------------------------
+auto shutdown_target_impl::do_decode_shutdown_request(
+  const message_context& msg_ctx,
+  const stored_message& message) noexcept -> std::optional<shutdown_request> {
+    typename shutdown_service_duration::rep count{0};
+    if(default_deserialize(count, message.content())) {
+        const shutdown_service_duration ticks{count};
+        const typename shutdown_service_clock::time_point ts{ticks};
+        const auto age{this->now() - ts};
+        return {shutdown_request{
+          .source_id = message.source_id,
+          .age = std::chrono::duration_cast<std::chrono::milliseconds>(age),
+          .verified = base.verify_bits(message)}};
+    }
+    return {};
+}
+//------------------------------------------------------------------------------
+auto shutdown_target_impl::decode_shutdown_request(
+  const message_context& msg_ctx,
+  const stored_message& message) noexcept -> std::optional<shutdown_request> {
+    if(msg_ctx.msg_id().is("Shutdown", "shutdown")) {
+        return do_decode_shutdown_request(msg_ctx, message);
+    }
+    return {};
+}
 //------------------------------------------------------------------------------
 auto make_shutdown_target_impl(subscriber& base, shutdown_target_signals& sigs)
   -> unique_holder<shutdown_target_intf> {
