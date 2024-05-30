b355d889bde7674635c46f38ee4a432a5f5433b7
Matus Chochlik <chochlik@gmail.com>
2024-05-30 07:17:31 +0200

    Small additions to the PAHO MQTT connection + updated core sub-module

--- source/modules/eagine/core/paho_mqtt_impl.cpp
+++ source/modules/eagine/core/paho_mqtt_impl.cpp
@@ -20,6 +20,7 @@ import eagine.core.memory;
 import eagine.core.string;
 import eagine.core.identifier;
 import eagine.core.utility;
+import eagine.core.runtime;
 import eagine.core.valid_if;
 import eagine.core.main_ctx;
 
@@ -47,9 +48,7 @@ class paho_mqtt_connection
   : public main_ctx_object
   , public paho_mqtt_connection_info<connection> {
 public:
-    paho_mqtt_connection(
-      main_ctx_parent parent,
-      const string_view addr_str) noexcept;
+    paho_mqtt_connection(main_ctx_parent parent, const url& locator) noexcept;
 
     using fetch_handler = connection::fetch_handler;
 
@@ -72,6 +71,9 @@ public:
     auto routing_weight() noexcept -> float final;
 
 private:
+    auto _get_broker_url(const url&) noexcept -> std::string;
+    auto _get_client_uid(const url&) noexcept -> std::string;
+
     auto _qos() const noexcept -> int;
 
     auto _add_subscription(string_view) noexcept -> bool;
@@ -85,6 +87,9 @@ private:
     static auto _message_arrived_f(void*, char*, int, MQTTClient_message*)
       -> int;
     static void _connection_lost_f(void*, char*);
+
+    const std::string _broker_url;
+    const std::string _client_uid;
 
     ::MQTTClient _mqtt_client{};
     bool _created : 1 {false};
@@ -157,10 +162,30 @@ auto paho_mqtt_connection::_remove_subscription(string_view) noexcept -> bool {
     return false;
 }
 //------------------------------------------------------------------------------
+auto paho_mqtt_connection::_get_broker_url(const url& locator) noexcept
+  -> std::string {
+    return std::format(
+      "tcp://{:s}:${:d}",
+      locator.domain().value_or("localhost").std_span(),
+      locator.port().value_or(1883));
+}
+//------------------------------------------------------------------------------
+auto paho_mqtt_connection::_get_client_uid(const url& locator) noexcept
+  -> std::string {
+    if(const auto uid{locator.login()}) {
+        return *uid;
+    }
+    std::string result{'_', 16Z};
+    main_context().random_identifier(result);
+    return result;
+}
+//------------------------------------------------------------------------------
 paho_mqtt_connection::paho_mqtt_connection(
   main_ctx_parent parent,
-  const string_view) noexcept
-  : main_ctx_object{"PahoMQTTCn", parent} {}
+  const url& locator) noexcept
+  : main_ctx_object{"PahoMQTTCn", parent}
+  , _broker_url{_get_broker_url(locator)}
+  , _client_uid{_get_client_uid(locator)} {}
 //------------------------------------------------------------------------------
 auto paho_mqtt_connection::update() noexcept -> work_done {
     return {};
@@ -228,7 +253,7 @@ auto paho_mqtt_connection_factory::make_acceptor(const string_view) noexcept
 //------------------------------------------------------------------------------
 auto paho_mqtt_connection_factory::make_connector(
   const string_view addr_str) noexcept -> shared_holder<connection> {
-    return {hold<paho_mqtt_connection>, *this, addr_str};
+    return {hold<paho_mqtt_connection>, *this, url{to_string(addr_str)}};
 }
 //------------------------------------------------------------------------------
 // Factory functions
