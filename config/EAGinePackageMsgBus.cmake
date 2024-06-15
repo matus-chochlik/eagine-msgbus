aebf3bdf63c492b2bc73783f3cb7df3e1d8187b3
Matus Chochlik <chochlik@gmail.com>
2024-06-15 07:44:28 +0200

    Changed how packages are built

---
+++ config/EAGinePackageMsgBus.cmake
@@ -0,0 +1,14 @@
+#  Copyright Matus Chochlik.
+#  Distributed under the Boost Software License, Version 1.0.
+#  See accompanying file LICENSE_1_0.txt or copy at
+#  https://www.boost.org/LICENSE_1_0.txt
+#
+# Package specific options
+#  Debian
+#   Dependencies
+set(CPACK_DEBIAN_MSGBUS-APPS_PACKAGE_DEPENDS "eagine-core-tools,eagine-core-user,libsystemd0,libc++1-17")
+set(CPACK_DEBIAN_MSGBUS-DEV_PACKAGE_DEPENDS "eagine-core-dev (>= @EAGINE_VERSION@),eagine-sslplus-dev (>= @EAGINE_VERSION@)")
+#   Descriptions
+set(CPACK_DEBIAN_-MSGBUS-APPS_DESCRIPTION "Collection of EAGine msgbus applications.")
+set(CPACK_DEBIAN_-MSGBUS-DEV_DESCRIPTION "C++ implementation of an inter-process network message bus.")
+
