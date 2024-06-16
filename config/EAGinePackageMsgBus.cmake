db2134093674655c53e238f7217292b2f001405b
Matus Chochlik <chochlik@gmail.com>
2024-06-16 11:27:24 +0200

    Added the examples install component + updated sub-modules

--- config/EAGinePackageMsgBus.cmake
+++ config/EAGinePackageMsgBus.cmake
@@ -6,9 +6,12 @@
 # Package specific options
 #  Debian
 #   Dependencies
-set(CPACK_DEBIAN_MSGBUS-APPS_PACKAGE_DEPENDS "eagine-core-tools,eagine-core-user,libsystemd0,libc++1-17")
+set(CXX_RUNTIME_PKGS "libc6,libc++1-17")
+set(CPACK_DEBIAN_MSGBUS-APPS_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g,libssl3,eagine-core-tools,eagine-core-user")
+set(CPACK_DEBIAN_MSGBUS-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g,libssl3,eagine-core-tools,eagine-core-user")
 set(CPACK_DEBIAN_MSGBUS-DEV_PACKAGE_DEPENDS "eagine-core-dev (>= @EAGINE_VERSION@),eagine-sslplus-dev (>= @EAGINE_VERSION@)")
 #   Descriptions
 set(CPACK_DEBIAN_MSGBUS-APPS_DESCRIPTION "Collection of EAGine msgbus applications.")
+set(CPACK_DEBIAN_MSGBUS-EXAMPLES_DESCRIPTION "EAGine MsgBus examples.")
 set(CPACK_DEBIAN_MSGBUS-DEV_DESCRIPTION "C++ implementation of an inter-process network message bus.")
 
