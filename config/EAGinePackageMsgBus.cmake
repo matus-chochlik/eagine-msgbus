8336b69be86d7eb2200b94e7904ef43b9b49b855
Matus Chochlik <chochlik@gmail.com>
2024-06-19 10:58:41 +0200

    Changes in how package properties are specified + updated sub-modules

--- config/EAGinePackageMsgBus.cmake
+++ config/EAGinePackageMsgBus.cmake
@@ -7,11 +7,11 @@
 #  Debian
 #   Dependencies
 set(CXX_RUNTIME_PKGS "libc6,libc++1-17")
-set(CPACK_DEBIAN_MSGBUS-APPS_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g,libssl3,eagine-core-tools,eagine-core-user")
+set(CPACK_DEBIAN_MSGBUS-APPS_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g,libssl3")
 set(CPACK_DEBIAN_MSGBUS-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g,libssl3,eagine-core-tools,eagine-core-user")
-set(CPACK_DEBIAN_MSGBUS-DEV_PACKAGE_DEPENDS "eagine-core-dev (>= @EAGINE_VERSION@),eagine-sslplus-dev (>= @EAGINE_VERSION@)")
+set(CPACK_DEBIAN_MSGBUS-DEV_PACKAGE_DEPENDS "")
 #   Descriptions
-set(CPACK_DEBIAN_MSGBUS-APPS_DESCRIPTION "Collection of EAGine msgbus applications.")
+set(CPACK_DEBIAN_MSGBUS-APPS_DESCRIPTION "")
 set(CPACK_DEBIAN_MSGBUS-EXAMPLES_DESCRIPTION "EAGine MsgBus examples.")
-set(CPACK_DEBIAN_MSGBUS-DEV_DESCRIPTION "C++ implementation of an inter-process network message bus.")
+set(CPACK_DEBIAN_MSGBUS-DEV_DESCRIPTION "")
 
